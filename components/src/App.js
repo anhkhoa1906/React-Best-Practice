45fedde1093e060570af347d6a80bcf79331b5b6
KhoaAnh <anhkhoa30051529@gmai.com>
2025-02-12 21:08:39 +0700

    props

--- components/src/App.js
+++ components/src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
 import Header from './components/Header/Greeting';
+import Message from './components/Header/WelcomeMessage';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Message name="KhoaAnh" age={30}/>
     </div>
+   
   );
 }
+
 
 export default App;
---
+++ components/src/components/Header/WelcomeMessage.js
@@ -0,0 +1,9 @@
+function Message(prop){
+    return(
+        <div>
+            <p>Name: {prop.name}</p>
+            <p>Age: {prop.age}</p>
+        </div>
+    );
+}
+export default Message
