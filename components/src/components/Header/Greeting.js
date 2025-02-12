01393e3b9fa26c4efcfdc56e388392626b608ad9
KhoaAnh <anhkhoa30051529@gmai.com>
2025-02-12 19:06:57 +0700

    component

---
+++ components/src/components/Header/Greeting.js
@@ -0,0 +1,9 @@
+import{Component, component} from 'react';
+class Header extends Component{
+    render(){
+        return(
+            <h1>Hello, Anh Khoa</h1>
+        )
+    }
+}
+export default Header;
